f1e24ce136411b3dd22dde883648493c31156fdd
"Artem Ilinykh" <"devsinglesly@gmail.com">
2022-06-10 17:43:50 +0600

    restrict token transfer impl

---
+++ contracts/BEP20TokenRestrictTransfer.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+import "./BEP20Token.sol";
+
+contract BEP20TokenRestrictTransfer is BEP20Token {
+    using SafeMath for uint256;
+
+    bool private _enable = false;
+
+    function _beforeTokenTransfer(address sender, address recipient, uint256 amount) internal override {
+        if(_enable) {
+            return;
+        }
+
+        if(isOwner(sender)) {
+            return;
+        }
+
+        revert("Cannot transfer");
+    }
+
+    function enableTransfer() public onlyOwner {
+        _enable = true;
+    }
+
+    function disableTransfer() public onlyOwner {
+        _enable = false;
+    }
+}
